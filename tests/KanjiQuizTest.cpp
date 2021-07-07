e466cc6f28787a9b06e49ec127a9e0376e4d5986
Dan H <heberdan1@gmail.com>
2021-07-06 21:30:53 -0400

    added a couple quiz tests

--- tests/KanjiQuizTest.cpp
+++ tests/KanjiQuizTest.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
-#include <kanji/KanjiQuiz.h>
 #include <kanji/Kanji.h>
+#include <kanji/KanjiQuiz.h>
+
+#include <sstream>
 
 namespace kanji {
 
@@ -10,18 +12,71 @@ protected:
   static const char** argv() {
     static const char* arg0 = "testMain";
     static const char* arg1 = "../../data";
-    static const char* args[] = { arg0, arg1 };
+    static const char* args[] = {arg0, arg1};
     return args;
   }
   // Contructs KanjiData using the real data files
-  KanjiQuizTest() : _quiz(2, argv()) {}
+  KanjiQuizTest() : _quiz(2, argv(), _os, _es, _is) {}
 
+  void startGradeQuiz() {
+    _is << "g\n1\nb\n4\nk\n";
+    // the above statement passes a string to '_is' so that '_quiz' can read the follow options:
+    // 'g' for Grade Quiz
+    // '1' for Grade 1
+    // '4' for 4 choices
+    // 'k' for kanji to reading quiz
+  }
+  void quit() { _is << "/\n"; } // '/' is the option to quit
+  void skip() { _is << ".\n"; } // '.' is the option to skip
+
+  std::stringstream _os;
+  std::stringstream _es;
+  std::stringstream _is;
   KanjiQuiz _quiz;
 };
 
 TEST_F(KanjiQuizTest, GroupsLoaded) {
   EXPECT_FALSE(_quiz.meaningGroupList().empty());
   EXPECT_FALSE(_quiz.patternGroupList().empty());
+}
+
+TEST_F(KanjiQuizTest, StartGradeQuiz) {
+  startGradeQuiz();
+  quit();
+  _quiz.quiz();
+  std::string line, lastLine;
+  while (std::getline(_os, line))
+    lastLine = line;
+  // test the line sent to _os
+  EXPECT_EQ(lastLine, "Final score: 0/0");
+  // should be nothing sent to _es (for errors) and nothing left in _is
+  EXPECT_FALSE(std::getline(_es, line));
+  EXPECT_FALSE(std::getline(_is, line));
+}
+
+TEST_F(KanjiQuizTest, SkipQuestions) {
+  for (int i = 2; i < 4; ++i) {
+    startGradeQuiz();
+    for (int j = 0; j < i; ++j)
+      skip();
+    quit();
+    _quiz.quiz();
+    std::string line, lastLine;
+    while (std::getline(_os, line))
+      lastLine = line;
+    auto expected = "Final score: 0/" + std::to_string(i) + ", skipped: " + std::to_string(i);
+    EXPECT_EQ(lastLine, expected);
+    // need to clear eofbit and failbit after reading to the end
+    EXPECT_FALSE(_os.good());
+    EXPECT_TRUE(_os.eof());
+    EXPECT_TRUE(_os.fail());
+    EXPECT_FALSE(_os.bad());
+    _os.clear();
+    EXPECT_TRUE(_os.good());
+    EXPECT_FALSE(_os.eof());
+    EXPECT_FALSE(_os.fail());
+    EXPECT_FALSE(_os.bad());
+  }
 }
 
 } // namespace kanji
