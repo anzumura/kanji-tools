5cfb75cb3c2b7f03b8e48959cc75991fcd0d725b
Dan H <9552296+anzumura@users.noreply.github.com>
2022-02-04 10:57:16 -0500

    added to_underlying function

--- include/kanji_tools/utils/EnumBitmask.h
+++ include/kanji_tools/utils/EnumBitmask.h
@@ -13,27 +13,30 @@ namespace kanji_tools {
 // 'MyEnum' must be a scoped enum with values set to powers of 2. The specialization enables the 7
 // required bitwise operators plus a 'hasValue' function (see EnumBitmaskTest.cpp for examples).
 
-// C++ 23 should include 'is_scoped_enum' so the following code can be removed:
+// C++ 23 should include 'is_scoped_enum' and 'to_underlying' so the following code can be removed:
 template<typename T, bool B = std::is_enum_v<T>> struct is_scoped_enum : std::false_type {};
 template<typename T>
 struct is_scoped_enum<T, true> : std::integral_constant<bool, !std::is_convertible_v<T, std::underlying_type_t<T>>> {};
 template<typename T> inline constexpr bool is_scoped_enum_v = is_scoped_enum<T>::value;
+template<typename T, std::enable_if_t<is_scoped_enum_v<T>, int> = 0> constexpr auto to_underlying(T x) {
+  return static_cast<std::underlying_type_t<T>>(x);
+}
 
 // 'is_bitmask' bool that should be specialized:
 template<typename T, std::enable_if_t<is_scoped_enum_v<T>, int> = 0> inline constexpr bool is_bitmask = false;
 
 // the 7 required bitwise operators: &, |, ^, ~, &=, |= and ^=
 template<typename T, std::enable_if_t<is_bitmask<T>, int> = 0> [[nodiscard]] constexpr T operator&(T x, T y) noexcept {
-  return static_cast<T>(static_cast<std::underlying_type_t<T>>(x) & static_cast<std::underlying_type_t<T>>(y));
+  return static_cast<T>(to_underlying(x) & to_underlying(y));
 }
 template<typename T, std::enable_if_t<is_bitmask<T>, int> = 0> [[nodiscard]] constexpr T operator|(T x, T y) noexcept {
-  return static_cast<T>(static_cast<std::underlying_type_t<T>>(x) | static_cast<std::underlying_type_t<T>>(y));
+  return static_cast<T>(to_underlying(x) | to_underlying(y));
 }
 template<typename T, std::enable_if_t<is_bitmask<T>, int> = 0> [[nodiscard]] constexpr T operator^(T x, T y) noexcept {
-  return static_cast<T>(static_cast<std::underlying_type_t<T>>(x) ^ static_cast<std::underlying_type_t<T>>(y));
+  return static_cast<T>(to_underlying(x) ^ to_underlying(y));
 }
 template<typename T, std::enable_if_t<is_bitmask<T>, int> = 0> [[nodiscard]] constexpr T operator~(T x) noexcept {
-  return static_cast<T>(~static_cast<std::underlying_type_t<T>>(x));
+  return static_cast<T>(~to_underlying(x));
 }
 template<typename T, std::enable_if_t<is_bitmask<T>, int> = 0> constexpr T& operator&=(T& x, T y) noexcept {
   x = x & y;
@@ -50,7 +53,7 @@ template<typename T, std::enable_if_t<is_bitmask<T>, int> = 0> constexpr T& oper
 
 // 'hasValue' can help in cases like 'if (hasValue(myEnum & MyEnum::Flag1)) ...'
 template<typename T, std::enable_if_t<is_bitmask<T>, int> = 0> [[nodiscard]] constexpr bool hasValue(T x) noexcept {
-  return static_cast<std::underlying_type_t<T>>(x);
+  return to_underlying(x);
 }
 
 } // namespace kanji_tools
