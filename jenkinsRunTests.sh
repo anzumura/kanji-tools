7a423e3f83285305a0dcaeae682249a0dd488eb1
Dan H <9552296+anzumura@users.noreply.github.com>
2022-03-24 08:29:34 -0400

    add more comments

--- jenkinsRunTests.sh
+++ jenkinsRunTests.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
-# add this script as an 'Execute shell' build step after the actual build
+# add this script as an 'Execute shell' build step after the actual build to
+# generate reports for Jenkins post build actions (JUnit and Cobertura)
 
 # Example 'Execute shell' build commands for a 'Clang' 'Debug' build:
 #
-# mkdir -p build
-# /opt/homebrew/bin/cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
-#   -DCMAKE_BUILD_TYPE:STRING=Debug \
-#   -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++ \
-#   -S"$WORKSPACE" -B"$WORKSPACE/build" -G "Unix Makefiles"
-# /opt/homebrew/bin/cmake --build build -j 10
+# cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -Bbuild
+# cmake --build build -j 10
 
-# remove line-count results from previous test coverage runs
+# Some global Jenkins env vars that can help the build are:
+#   PATH=/opt/homebrew/bin:$PATH
+#   CMAKE_GENERATOR="Unix Makefiles"
+#   CMAKE_EXPORT_COMPILE_COMMANDS=TRUE
+
+# remove coverage results from previous runs
 find build/src -name '*.gcda' -exec rm {} \;
 
-cd build/tests
 declare -r r=../../..
+cd build/tests
 for i in *; do
   cd $i
-
-  # generate report for 'Publish JUnit test result' post build action
   ./${i}Test --gtest_output="xml:results.xml"
-  # set 'Test report XMLs' to: build/tests/*/results.xml
-
-  # generate report for 'Publish Cobertura Coverage' post build action
-  /opt/homebrew/bin/gcovr -x -r $r -f $r/src -f $r/include > coverage.xml
-  # set 'Cobertura xml report pattern' to: build/tests/*/coverage.xml
-
+  gcovr -x -r$r -f$r/src -f$r/include > coverage.xml
   cd ..
 done
+
+# set the following values for the actions:
+# - Publish JUnit test result:
+#   set 'Test report XMLs' to: 'build/tests/*/results.xml'
+# - Publish Cobertura Coverage:
+#   set 'Cobertura xml report pattern' to: 'build/tests/*/coverage.xml'
