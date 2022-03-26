178240d45084bbd828c51fdbe3f6f2061d7f6181
Dan H <9552296+anzumura@users.noreply.github.com>
2022-03-26 17:12:19 -0400

    add more cases for findDakuten and findHanDakuten

--- tests/kana/KanaTest.cpp
+++ tests/kana/KanaTest.cpp
@@ -65,12 +65,14 @@ TEST(KanaTest, FindDakuten) {
   EXPECT_EQ(Kana::findDakuten("シ"), Kana::OptString{"ジ"});
   EXPECT_EQ(Kana::findDakuten("う"), Kana::OptString{"ゔ"});
   EXPECT_EQ(Kana::findDakuten("ま"), Kana::EmptyOptString);
+  EXPECT_EQ(Kana::findDakuten("マ"), Kana::EmptyOptString);
 }
 
 TEST(KanaTest, FindHanDakuten) {
   EXPECT_EQ(Kana::findHanDakuten("は"), Kana::OptString{"ぱ"});
   EXPECT_EQ(Kana::findHanDakuten("ホ"), Kana::OptString{"ポ"});
   EXPECT_EQ(Kana::findHanDakuten("さ"), Kana::EmptyOptString);
+  EXPECT_EQ(Kana::findHanDakuten("サ"), Kana::EmptyOptString);
 }
 
 TEST(KanaTest, CheckHiragana) {
