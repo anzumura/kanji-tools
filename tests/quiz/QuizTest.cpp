103d1a7d3e26327a5110b510beefac90218679f0
Dan H <9552296+anzumura@users.noreply.github.com>
2022-03-29 14:45:25 -0400

    add info and debug tests for Quiz program

---
+++ tests/quiz/QuizTest.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <kanji_tools/kanji/KanjiData.h>
+#include <kanji_tools/quiz/QuizLauncher.h>
+
+#include <sstream>
+
+namespace kanji_tools {
+
+TEST(QuizTest, Info) {
+  const char* args[]{"", "-info"};
+  std::stringstream os;
+  const auto data{std::make_shared<KanjiData>(std::size(args), args, os)};
+  const auto groupData{std::make_shared<GroupData>(data)};
+  const auto jukugoData{std::make_shared<JukugoData>(data)};
+  QuizLauncher quiz{0, {}, data, groupData, jukugoData};
+  // look for a few strings instead of comparing the whole output
+  const auto expected = {">>> Loaded 1460 kanji into 88 groups",
+      ">>> Loaded 5700 kanji into 1038 groups",
+      ">>> Total Kanji with Jukugo: 2910, unique jukugo: 18490"};
+  size_t found{};
+  for (std::string line; std::getline(os, line);)
+    for (const auto& i : expected)
+      if (line == i) {
+        ++found;
+        break;
+      }
+  EXPECT_EQ(found, std::size(expected));
+}
+
+TEST(QuizTest, Debug) {
+  const char* args[]{"", "-debug"};
+  std::stringstream os;
+  const auto data{std::make_shared<KanjiData>(std::size(args), args, os)};
+  const auto groupData{std::make_shared<GroupData>(data)};
+  const auto jukugoData{std::make_shared<JukugoData>(data)};
+  QuizLauncher quiz{0, {}, data, groupData, jukugoData};
+  // look for a few strings instead of comparing the whole output
+  const auto expected = {"阿：ア( 3)   阿': 婀# 痾#",
+      "時間：十干 (10)   : 甲. 乙. 丙. 丁. 戊^ 己. 庚^ 辛. 壬\" 癸+",
+      "畏：ワイ、イ( 3)   畏.: 隈\" 猥#"};
+  size_t found{};
+  for (std::string line; std::getline(os, line);)
+    for (const auto& i : expected)
+      if (line.ends_with(i)) {
+        ++found;
+        break;
+      }
+  EXPECT_EQ(found, std::size(expected));
+}
+
+} // namespace kanji_tools
