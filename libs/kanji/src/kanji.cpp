f3316c4b23e5910780211e3c06d0ba50ec61f5f3
Dan H <9552296+anzumura@users.noreply.github.com>
2022-05-11 19:29:36 -0400

    clang coverage problem

--- libs/kanji/src/kanji.cpp
+++ libs/kanji/src/kanji.cpp
@@ -124,7 +124,7 @@ uint16_t Kanji::qualifiedNameRank() const { // NOLINT
          : t == Extra              ? vExt
          : t == Ucd                ? vUcd
          : kyu() != KenteiKyus::K1 ? vNoK1
-                                   : vK1;
+                                   : vK1; // LCOV_EXCL_LINE: covered
 }
 
 } // namespace kanji_tools
