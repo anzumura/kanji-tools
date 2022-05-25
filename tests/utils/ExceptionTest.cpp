3a42cf922ef3402ff39cc7a24cc8b2a2084b9c04
Dan H <9552296+anzumura@users.noreply.github.com>
2022-05-25 16:58:25 -0400

    add tests for exception classes

---
+++ tests/utils/ExceptionTest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include <kanji_tools/utils/Exception.h>
+
+namespace kanji_tools {
+
+TEST(ExceptionTest, DomainErrorCtors) {
+  const auto charMsg{"testMsg"};
+  const String msg{charMsg};
+  EXPECT_STREQ(charMsg, DomainError{charMsg}.what());
+  EXPECT_EQ(msg, DomainError{msg}.what());
+}
+
+TEST(ExceptionTest, RangeErrorCtors) {
+  const auto charMsg{"testMsg"};
+  const String msg{charMsg};
+  EXPECT_STREQ(charMsg, RangeError{charMsg}.what());
+  EXPECT_EQ(msg, RangeError{msg}.what());
+}
+
+} // namespace kanji_tools
