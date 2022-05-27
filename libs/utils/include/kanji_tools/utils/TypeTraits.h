954d2e097ea0bba8af8a88ca65af837464fb5d9c
Dan H <9552296+anzumura@users.noreply.github.com>
2022-05-27 17:17:02 -0400

    doxygen comments

--- libs/utils/include/kanji_tools/utils/TypeTraits.h
+++ libs/utils/include/kanji_tools/utils/TypeTraits.h
@@ -2,34 +2,35 @@
 
 #include <type_traits>
 
-namespace kanji_tools {
+namespace kanji_tools { /// \utils_group{TypeTraits}
 
-// C++ 23 should include 'is_scoped_enum' and 'to_underlying' so the following
-// code can be removed:
-
+/// struct for testing if `T` is a scoped enum (will be included in 'C++23')
 template<typename T, bool B = std::is_enum_v<T>>
 struct is_scoped_enum : std::false_type {};
 
+/// `value` member is true if `T` is a scoped enum (will be included 'C++ 23')
 template<typename T>
 struct is_scoped_enum<T, true>
     : std::integral_constant<bool,
           !std::is_convertible_v<T, std::underlying_type_t<T>>> {};
 
+/// true if `T` is a scoped enum (will be included 'C++ 23')
 template<typename T>
 inline constexpr auto is_scoped_enum_v{is_scoped_enum<T>::value};
 
+/// return the underlying value of `x` (will be included 'C++ 23')
 template<typename T, std::enable_if_t<is_scoped_enum_v<T>, int> = 0>
 [[nodiscard]] constexpr auto to_underlying(T x) noexcept {
   return static_cast<std::underlying_type_t<T>>(x);
 }
 
-// cast from a scoped enum's underlying type back to the enum - this is safer
-// than a plain static_cast since T must be a scoped enum and the function param
-// is of the corresponding underlying type for T
+/// cast `u` to a scoped enum - this is safer than using `static_cast` since `T`
+/// must be a scoped enum and `u` has the actual underlying type for `T`
 template<typename T>
 [[nodiscard]] constexpr std::enable_if_t<is_scoped_enum_v<T>, T> to_enum(
     std::underlying_type_t<T> u) noexcept {
   return static_cast<T>(u);
 }
 
+/// \end_group
 } // namespace kanji_tools
