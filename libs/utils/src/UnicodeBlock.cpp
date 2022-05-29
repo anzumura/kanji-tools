f986519ac333c0b97d7da4e6de0206a810eff1db
Dan H <9552296+anzumura@users.noreply.github.com>
2022-05-29 16:15:17 -0400

    re-ordered code to match header file

--- libs/utils/src/UnicodeBlock.cpp
+++ libs/utils/src/UnicodeBlock.cpp
@@ -18,81 +18,79 @@ std::ostream& operator<<(std::ostream& os, const UnicodeBlock& b) {
   return os;
 }
 
-// Kana
-
-bool isHiragana(const String& s, bool sizeOne) {
-  return inWCharRange(s, sizeOne, HiraganaBlocks);
-}
-
-bool isAllHiragana(const String& s) { return inWCharRange(s, HiraganaBlocks); }
-
-bool isKatakana(const String& s, bool sizeOne) {
-  return inWCharRange(s, sizeOne, KatakanaBlocks);
-}
-
-bool isAllKatakana(const String& s) { return inWCharRange(s, KatakanaBlocks); }
+// 'is' functions
 
 bool isKana(const String& s, bool sizeOne) {
   return inWCharRange(s, sizeOne, HiraganaBlocks, KatakanaBlocks);
 }
 
-bool isAllKana(const String& s) {
-  return inWCharRange(s, HiraganaBlocks, KatakanaBlocks);
+bool isHiragana(const String& s, bool sizeOne) {
+  return inWCharRange(s, sizeOne, HiraganaBlocks);
 }
 
-// Kanji
-
-bool isCommonKanji(const String& s, bool sizeOne) {
-  return inWCharRange(s, sizeOne, CommonKanjiBlocks);
-}
-
-bool isAllCommonKanji(const String& s) {
-  return inWCharRange(s, CommonKanjiBlocks);
-}
-
-bool isRareKanji(const String& s, bool sizeOne) {
-  return inWCharRange(s, sizeOne, RareKanjiBlocks);
-}
-
-bool isAllRareKanji(const String& s) {
-  return inWCharRange(s, RareKanjiBlocks);
+bool isKatakana(const String& s, bool sizeOne) {
+  return inWCharRange(s, sizeOne, KatakanaBlocks);
 }
 
 bool isKanji(const String& s, bool sizeOne) {
   return inWCharRange(s, sizeOne, CommonKanjiBlocks, RareKanjiBlocks);
 }
 
-bool isAllKanji(const String& s) {
-  return inWCharRange(s, CommonKanjiBlocks, RareKanjiBlocks);
+bool isCommonKanji(const String& s, bool sizeOne) {
+  return inWCharRange(s, sizeOne, CommonKanjiBlocks);
 }
 
-// other multi-byte characters
-
-bool isMBPunctuation(const String& s, bool includeSpace, bool sizeOne) {
-  return s.starts_with("　") ? (includeSpace && (s.size() < 4 || !sizeOne))
-                             : inWCharRange(s, sizeOne, PunctuationBlocks);
-}
-
-bool isAllMBPunctuation(const String& s) {
-  return inWCharRange(s, PunctuationBlocks);
+bool isRareKanji(const String& s, bool sizeOne) {
+  return inWCharRange(s, sizeOne, RareKanjiBlocks);
 }
 
 bool isMBSymbol(const String& s, bool sizeOne) {
   return inWCharRange(s, sizeOne, SymbolBlocks);
 }
 
-bool isAllMBSymbol(const String& s) { return inWCharRange(s, SymbolBlocks); }
-
 bool isMBLetter(const String& s, bool sizeOne) {
   return inWCharRange(s, sizeOne, LetterBlocks);
 }
 
-bool isAllMBLetter(const String& s) { return inWCharRange(s, LetterBlocks); }
+bool isMBPunctuation(const String& s, bool includeSpace, bool sizeOne) {
+  return s.starts_with("　") ? (includeSpace && (s.size() < 4 || !sizeOne))
+                             : inWCharRange(s, sizeOne, PunctuationBlocks);
+}
 
 bool isRecognizedUtf8(const String& s, bool sizeOne) {
   return inWCharRange(s, sizeOne, HiraganaBlocks, CommonKanjiBlocks,
       RareKanjiBlocks, KatakanaBlocks, PunctuationBlocks, SymbolBlocks,
       LetterBlocks);
+}
+
+// 'isAll' functions
+
+bool isAllKana(const String& s) {
+  return inWCharRange(s, HiraganaBlocks, KatakanaBlocks);
+}
+
+bool isAllHiragana(const String& s) { return inWCharRange(s, HiraganaBlocks); }
+
+bool isAllKatakana(const String& s) { return inWCharRange(s, KatakanaBlocks); }
+
+bool isAllKanji(const String& s) {
+  return inWCharRange(s, CommonKanjiBlocks, RareKanjiBlocks);
+}
+
+bool isAllCommonKanji(const String& s) {
+  return inWCharRange(s, CommonKanjiBlocks);
+}
+
+bool isAllRareKanji(const String& s) {
+  return inWCharRange(s, RareKanjiBlocks);
+}
+
+bool isAllMBSymbol(const String& s) { return inWCharRange(s, SymbolBlocks); }
+
+bool isAllMBLetter(const String& s) { return inWCharRange(s, LetterBlocks); }
+
+bool isAllMBPunctuation(const String& s) {
+  return inWCharRange(s, PunctuationBlocks);
 }
 
 bool isAllRecognizedUtf8(const String& s) {
